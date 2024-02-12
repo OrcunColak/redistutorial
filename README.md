9b903716c3683cc17d5ba66aa45c051b947a3783
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-12 16:09:50 +0300

    consumer group example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# redistutorial
-Docker compose and docker files for redis
+# Consumer For Stream
+
+The original idea is from
+
+https://medium.com/@tarunannapareddy1997/demystifying-redis-messaging-0448e4dba9e0
---
+++ src/main/java/stream/consumer/SingleConsumer.java
@@ -0,0 +1,42 @@
+package stream.consumer;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.StreamMessage;
+import io.lettuce.core.XReadArgs;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisStreamCommands;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class SingleConsumer {
+
+    public static void main(String[] args) {
+        RedisClient redisClient = RedisClient.create("redis://127.0.0.1:6379");
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        RedisStreamCommands<String, String> streamCommands = connection.sync();
+
+        String streamName = "stream";
+        String lastSeenOffset = "0-0";
+
+        while (true) {
+            XReadArgs count = XReadArgs.Builder.block(500).count(1);
+            XReadArgs.StreamOffset<String> from = XReadArgs.StreamOffset.from(streamName, lastSeenOffset);
+
+            List<StreamMessage<String, String>> streamMessages = streamCommands.xread(count, from);
+            for (StreamMessage<String, String> message : streamMessages) {
+                lastSeenOffset = message.getId();
+
+
+                Map<String, String> body = message.getBody();
+
+                System.out.print(lastSeenOffset + " received message");
+                System.out.print(" userId: " + body.get("userId"));
+                System.out.print(" order: " + body.get("order"));
+                System.out.println(" email: " + body.get("e-mail"));
+            }
+        }
+    }
+}
